ce2142c33bffa13ef03807d1982f2fdb0bee59f8
Jeff Pennington <jeffpenningtonnc@gmail.com>
2022-02-24 12:46:25 -0500

    fixes for deployment

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:video_player/video_player.dart';
+import '../Util/globals.dart';
 import '../Widget/title_bar.dart';
-import 'package:image_slider/image_slider.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key key, this.drawerKey}) : super(key: key);
@@ -12,102 +12,196 @@ class HomeView extends StatefulWidget {
   _HomeViewState createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView>
-    with SingleTickerProviderStateMixin {
+class _HomeViewState extends State<HomeView> {
+  VideoPlayerController controller;
+
   @override
   void initState() {
+    loadVideoPlayer();
     super.initState();
-    tabController = TabController(length: 4, vsync: this);
   }
 
-  TabController tabController;
-  WebViewController _controller;
-  bool _loading = true;
-
-  // static List<String> links = [
-  //   "https://admin.feedthehungerapp.com/images/slider/slider1.png",
-  //   "https://admin.feedthehungerapp.com/images/slider/slider2.png",
-  //   "https://admin.feedthehungerapp.com/images/slider/slider3.png",
-  //   "https://admin.feedthehungerapp.com/images/slider/slider4.png"
-  // ];
-
-  static List<String> links = [
-    "images/slider/slider1.png",
-    "images/slider/slider2.png",
-    "images/slider/slider3.png",
-    "images/slider/slider4.png"
-  ];
+  void loadVideoPlayer() {
+    controller = VideoPlayerController.asset('images/fth-drawing.mp4');
+    controller.addListener(() {
+      setState(() {});
+    });
+    controller.initialize().then((value) {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: <Widget>[
-            TitleBar(widget.key, "Welcome!"),
-            Container(
-              color: Colors.black,
-              child: ImageSlider(
-                showTabIndicator: false,
-
-                /// Customize tab's colors
-                tabIndicatorColor: Colors.white70,
-
-                /// Customize selected tab's colors
-                tabIndicatorSelectedColor: Colors.yellow,
-
-                /// Height of the indicators from the bottom
-                tabIndicatorHeight: 16,
-
-                /// Size of the tab indicator circles
-                tabIndicatorSize: 16,
-
-                /// tabController for walkthrough or other implementations
-                tabController: tabController,
-
-                /// Animation curves of sliding
-                curve: Curves.easeIn,
-
-                /// Width of the slider
-                width: MediaQuery.of(context).size.width,
-
-                /// Height of the slider
-                height: 220,
-
-                /// If automatic sliding is required
-                autoSlide: true,
-
-                /// Time for automatic sliding
-                duration: const Duration(seconds: 6),
-
-                /// If manual sliding is required
-                allowManualSlide: true,
-                children: links.map((String link) {
-                  return Image.asset(
-                    link,
-                    width: MediaQuery.of(context).size.width,
-                    fit: BoxFit.fitWidth,
-                  );
-                }).toList(),
+        body: Container(
+      color: const Color.fromARGB(25, 100, 100, 100),
+      child: Column(
+        children: <Widget>[
+          TitleBar(widget.key, "Welcome!"),
+          Stack(
+            children: [
+              AspectRatio(
+                aspectRatio: controller.value.aspectRatio,
+                child: Stack(
+                  children: [
+                    !controller.value.isPlaying
+                        ? GestureDetector(
+                            onTap: () {
+                              controller.play();
+                              setState(() {});
+                            },
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                        "images/fth-drawing-thumb.png")),
+                              ),
+                            ),
+                          )
+                        : GestureDetector(
+                            onTap: () {
+                              controller.pause();
+                              controller.seekTo(Duration.zero);
+                              setState(() {});
+                            },
+                            child: VideoPlayer(controller),
+                          ),
+                  ],
+                ),
               ),
+            ],
+          ),
+          Container(
+            color: Colors.black,
+            height: 2,
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 18, 16, 8),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            constraints: const BoxConstraints(
+                              minWidth: 115,
+                              minHeight: 115,
+                              maxWidth: 115,
+                              maxHeight: 115,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset("images/devotions.png"),
+                            ),
+                          ),
+                          Container(
+                            child: Flexible(
+                              child: Column(
+                                children: const [
+                                  Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Padding(
+                                      padding: EdgeInsets.fromLTRB(0, 8, 8, 8),
+                                      child: Text("Daily Devotions",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 24,
+                                          )
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    "Subscribe to receive 30 days of devotions right here within the Feed The Hunger App.",
+                                  ),
+                                  Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text("Subscribe Now >",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 12, 16, 8),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            constraints: const BoxConstraints(
+                              minWidth: 115,
+                              minHeight: 115,
+                              maxWidth: 115,
+                              maxHeight: 115,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset("images/prayerlist.png"),
+                            ),
+                          ),
+                          Container(
+                            child: Flexible(
+                              child: Column(
+                                children: const [
+                                  Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Padding(
+                                      padding: EdgeInsets.fromLTRB(0, 8, 8, 8),
+                                      child: Text("Weekly Prayer List",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 24,
+                                          )
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    "Prayer is the critical link between God and His people. Would you consider supporting us in prayer?",
+                                  ),
+                                  Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text("Prayer Needs >",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      )),
+                ),
+              ],
             ),
-            Expanded(
-              child: WebView(
-                initialUrl: 'https://mailchi.mp/feedthehunger/prayer-2-20-22',
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (WebViewController webViewController) {
-                  _controller = webViewController;
-                },
-                onPageFinished: (finish) {
-
-                  _controller.evaluateJavascript("document.getElementById('templatePreheader').remove(); ");
-
-                  setState(() {
-                    _loading = false;
-                  });
-                },
-              ),
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    ));
   }
 }
